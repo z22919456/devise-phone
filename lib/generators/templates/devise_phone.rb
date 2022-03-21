d4164181857b99f4eb48210a77bb8af61ecaf651
z01241 <z01241@lawbroker.com.tw>
2022-03-21 20:53:24 +0800

    add config template

--- lib/generators/templates/devise_phone.rb
+++ lib/generators/templates/devise_phone.rb
@@ -1,3 +1,15 @@
+# frozen_string_literal: true
+
 Devise.setup do |config|
-  
+  # Configure OTP length.
+  # Default is 6
+  config.otp_length = 6
+
+  # Configure should generate prefix.
+  # How many letters need to ber generated, 0 or nil if prefix is not required. Default is 3
+  config.generate_prefix = 3
+
+  # Configure available time
+  # How many time available after generate. default is 10 minutes
+  config.otp_available_time = 10
 end
